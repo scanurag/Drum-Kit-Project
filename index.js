1930676395949d3e332eb09eb4ed34c6bf07acdd
LunaticPrakash <prakash.25gupta@gmail.com>
2021-05-15 17:34:13 +0530

    Approach 1 completed

---
+++ index.js
@@ -0,0 +1,51 @@
+function addingEventListener(){
+
+    let drums = document.querySelectorAll(".drum"); //array
+    let numDrums = drums.length; //7
+
+    for (let i=0;i<numDrums;i++){
+        let which_drum = drums[i].innerText;
+        drums[i].addEventListener("click",function(){ 
+            playSound(which_drum); }
+        );
+    }
+}
+
+function playSound(which_drum){
+
+    if (which_drum === "w"){
+        let a = new Audio("./sounds/tom-1.mp3");
+        a.play();
+    }
+
+    else if (which_drum === "a"){
+        let a = new Audio("./sounds/tom-2.mp3");
+        a.play();
+    }
+
+    else if (which_drum === "s"){
+        let a = new Audio("./sounds/tom-3.mp3");
+        a.play();
+    }
+
+    else if (which_drum === "d"){
+        let a = new Audio("./sounds/tom-4.mp3");
+        a.play();
+    }
+
+    else if (which_drum === "j"){
+        let a = new Audio("./sounds/snare.mp3");
+        a.play();
+    }
+
+    else if (which_drum === "k"){
+        let a = new Audio("./sounds/crash.mp3");
+        a.play();
+    }
+
+    else if (which_drum === "l"){
+        let a = new Audio("./sounds/kick-bass.mp3");
+        a.play();
+    }
+}
+addingEventListener();
